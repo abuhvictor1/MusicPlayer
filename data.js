eeb8ee0ab55a632d36adf2d63c3b2afeee6e1d24
victor abuh <abuhvictor92@gmail.com>
2022-09-19 03:21:11 +0100

    built a music player

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'waves',
+        path: 'music/waves.mp3',
+        artist: 'artist 1',
+        cover: 'images/tohsaka.png'
+    },
+    {
+        name: ' all the stars',
+        path: 'music/Allthestars.mp3',
+        artist: 'artist 2',
+        cover: 'images/rin.jpeg'
+    },
+    {
+        name: 'bank on it',
+        path: 'music/BankOnIt.mp3',
+        artist: 'artist 3',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'fireOnFire',
+        path: 'music/fireonfire.mp3',
+        artist: 'artist 4',
+        cover: 'images/jake.jpg'
+    },
+    {
+        name: 'moonknight',
+        path: 'music/manwithoutlove.mp3',
+        artist: 'artist 5',
+        cover: 'images/cover 5.png'
+    },
+]
